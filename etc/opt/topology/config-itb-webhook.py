27d61990cea2640182dd14c44c7715bcdd14f04c
Carl Edquist <edquist@cs.wisc.edu>
2018-11-20 13:59:53 -0600

    copy config-itb.py -> config-itb-webhook.py

---
+++ etc/opt/topology/config-itb-webhook.py
@@ -0,0 +1,12 @@
+#
+# DO NOT EDIT, your edits will be overwritten!
+# Managed by Puppet
+#
+
+
+GIT_SSH_KEY = "/etc/opt/topology/bitbucket"
+
+TOPOLOGY_DATA_DIR = "/var/cache/topology-itb/topology"
+CONTACT_DATA_DIR = "/var/cache/topology-itb/contact"
+
+INSTANCE_NAME = "ITB instance"
