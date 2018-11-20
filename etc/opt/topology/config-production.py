e9e4b39ab25e624b90b30356a1838492bbeb9b5a
Carl Edquist <edquist@cs.wisc.edu>
2018-11-20 13:59:17 -0600

    /etc/opt/topology configs

---
+++ etc/opt/topology/config-production.py
@@ -0,0 +1,12 @@
+#
+# DO NOT EDIT, your edits will be overwritten!
+# Managed by Puppet
+#
+
+
+GIT_SSH_KEY = "/etc/opt/topology/bitbucket"
+
+TOPOLOGY_DATA_DIR = "/var/cache/topology/topology"
+CONTACT_DATA_DIR = "/var/cache/topology/contact"
+
+LOGLEVEL=10  # logging.DEBUG
